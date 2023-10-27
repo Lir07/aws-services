a14fcc770c8b716053b4200b74535d6e4a69afb6
Lir07 <lirballata01@gmail.com>
2023-10-27 10:04:25 +0200

    Project added

---
+++ 6.S3/S3Playground/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+var s3Client = new AmazonS3Client();
+
+var getObjectRequest = new GetObjectRequest
+{
+    BucketName = "lirbucket",
+    Key = "files/movies.csv"
+};
+
+var response = await s3Client.GetObjectAsync(getObjectRequest);
+
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var text = Encoding.Default.GetString(memoryStream.ToArray());
+
+
+Console.WriteLine(text);
+
+
+
+
+
+
+
+
+
+
+/*
+await using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = "lirbucket",
+    Key = "files/movies.csv",
+    ContentType = "text/csv",
+    InputStream = inputStream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
+*/
